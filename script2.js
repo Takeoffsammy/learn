a5fa3c115565c681a8ad3865c1649a0e66912672
Takeoffsammy <110278775+Takeoffsammy@users.noreply.github.com>
2022-08-27 11:23:33 +0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,6 @@
+const fileUploader = document.getElementById('file-uploader');
+
+fileUploader.addEventListener('change', (event) => {
+  const files = event.target.files;
+  console.log('files', files);
+});
